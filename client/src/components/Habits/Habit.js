d3f21dda190dbab01043e35ca91e4f56594ebb19
kaleido <kaleido@gmail.com>
2019-05-28 07:56:08 +0900

    それぞれのhabitにマウスを載せると背景色が変わるように

--- client/src/components/Habits/Habit.js
+++ client/src/components/Habits/Habit.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Comment, Segment } from "semantic-ui-react";
+import "./Habits.css";
 
 const Habit = ({ habit }) => {
 	return (
 		<Link to={`/habit/${habit._id}`}>
-			<Segment style={{ margin: "3px" }}>
-				<Comment className="">
+			<Segment
+				padded
+				className="Habit"
+				raised
+				color="teal"
+				style={{ margin: "1em" }}>
+				<Comment>
 					<Comment.Avatar src={habit.creator.imageUrl} />
 					<Comment.Content>
-						<Comment.Author as="a">{habit.creator.username}</Comment.Author>
+						<Comment.Author>{habit.creator.username}</Comment.Author>
 						<Comment.Metadata>
 							<div>{habit.startDate}</div>
 						</Comment.Metadata>
