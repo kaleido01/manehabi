94b3417f48f1f89578c4bfa78a05125637c7b6cf
kaleido <kaleido@gmail.com>
2019-06-03 19:14:25 +0900

    arojagr

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-console.log("aaa");
+console.log("aa");
 const port = process.env.PORT || 4000;
 
 server.listen({ port }).then(({ url }) => {
