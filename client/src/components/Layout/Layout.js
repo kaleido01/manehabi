d2707b3642ca24beaf66203237ec94d2f9a8991d
kaleido <kaleido@gmail.com>
2019-06-05 07:40:14 +0900

    sideDrawerの改善

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Sidebar } from "semantic-ui-react";
 import SideDrawer from "./../SideDrawer/SideDrawer";
 import Navbar from "./../Navbar/Navbar";
 import "./Layout.css";
@@ -7,13 +8,13 @@ const Layout = ({ session, children }) => {
 	const [onSide, setOnSide] = useState(false);
 
 	return (
-		<Fragment>
+		<Sidebar.Pushable>
 			<Navbar onSide={setOnSide} />
 
 			<SideDrawer onSide={onSide} onHide={setOnSide} />
 
 			<div className="Layout">{children}</div>
-		</Fragment>
+		</Sidebar.Pushable>
 	);
 };
 
