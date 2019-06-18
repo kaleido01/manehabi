6e34fed8728b7f8a00212a0c286f020873d66add
kaleido <kaleido@gmail.com>
2019-06-18 21:29:44 +0900

    初期画面のloading

--- client/src/components/Habits/Habits.js
+++ client/src/components/Habits/Habits.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { Query } from "react-apollo";
 import { withRouter } from "react-router-dom";
 import { GET_ALL_HABITS } from "./../../queries";
@@ -8,11 +8,13 @@ import Loader from "../shered/Loader";
 import { Grid, Header, Comment, Segment } from "semantic-ui-react";
 import queryString from "query-string";
 import SearchHabits from "./SearchHabits";
+import { WelcomeLoader } from "./../shered/Loader";
 
 const Habits = ({ location, refetch }) => {
 	const [descending, setDescending] = useState("-1");
 	const [option, setOption] = useState("createdAt");
 	const [searchTerm, setSearchTerm] = useState("");
+	const [authLoading, setAuthLoading] = useState(false);
 
 	const query = {
 		descending,
@@ -23,7 +25,11 @@ const Habits = ({ location, refetch }) => {
 		const { token } = queryString.parse(location.search);
 		if (token) {
 			localStorage.setItem("token", token);
-			refetch();
+			setAuthLoading(true);
+			refetch().then(res => {
+				setAuthLoading(false);
+				console.log(res);
+			});
 		}
 	}, []);
 
@@ -48,41 +54,47 @@ const Habits = ({ location, refetch }) => {
 		});
 	};
 	return (
-		<Grid textAlign="center" centered>
-			<Grid.Column style={{ minWidth: "350px" }} width={8}>
-				<Segment>
-					<SearchHabits
-						setDescending={setDescending}
-						descending={descending}
-						option={option}
-						setOption={setOption}
-						searchTerm={searchTerm}
-						setSearchTerm={setSearchTerm}
-					/>
-					<Comment.Group>
-						<Header as="h3" dividing textAlign="center">
-							新着習慣一覧
-						</Header>
+		<Fragment>
+			{authLoading ? (
+				<WelcomeLoader />
+			) : (
+				<Grid textAlign="center" centered>
+					<Grid.Column style={{ minWidth: "350px" }} width={8}>
+						<Segment>
+							<SearchHabits
+								setDescending={setDescending}
+								descending={descending}
+								option={option}
+								setOption={setOption}
+								searchTerm={searchTerm}
+								setSearchTerm={setSearchTerm}
+							/>
+							<Comment.Group>
+								<Header as="h3" dividing textAlign="center">
+									新着習慣一覧
+								</Header>
 
-						<Query
-							query={GET_ALL_HABITS}
-							variables={{ offset: 0, limit: 5, ...query }}>
-							{({ data, fetchMore, loading }) => {
-								if (loading) return <Loader />;
-								const { habits, pageInfo } = data.getAllHabits;
-								return (
-									<HabitList
-										habits={habits}
-										pageInfo={pageInfo}
-										onLoadMore={() => onLoadMore(habits, fetchMore)}
-									/>
-								);
-							}}
-						</Query>
-					</Comment.Group>
-				</Segment>
-			</Grid.Column>
-		</Grid>
+								<Query
+									query={GET_ALL_HABITS}
+									variables={{ offset: 0, limit: 5, ...query }}>
+									{({ data, fetchMore, loading }) => {
+										if (loading || authLoading) return <Loader />;
+										const { habits, pageInfo } = data.getAllHabits;
+										return (
+											<HabitList
+												habits={habits}
+												pageInfo={pageInfo}
+												onLoadMore={() => onLoadMore(habits, fetchMore)}
+											/>
+										);
+									}}
+								</Query>
+							</Comment.Group>
+						</Segment>
+					</Grid.Column>
+				</Grid>
+			)}
+		</Fragment>
 	);
 };
 
